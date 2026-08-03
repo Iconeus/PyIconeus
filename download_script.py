0fc4b8062ed70fb3a1d82aee1b0c03f8227c0f6f
simoncampredon-lang <simon.campredon@iconeus.com>
2026-08-03 12:56:54 +0200

    feat(CI): Data download script

---
+++ download_script.py
@@ -0,0 +1,15 @@
+import urllib.request, zipfile, os
+
+ZENODO_URL = "https://zenodo.org/records/21700212/files/data.zip"
+
+def download_test_data(dest="tests/data"):
+    if os.path.exists(dest):
+        return
+    print("Downloading test data...")
+    urllib.request.urlretrieve(ZENODO_URL, "data.zip")
+    with zipfile.ZipFile("data.zip") as z:
+        z.extractall("tests")
+    os.remove("data.zip")
+
+if __name__ == '__main__':
+    download_test_data()
